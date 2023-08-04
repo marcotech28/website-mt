01ab45039d8b611641c06355541ae030976a3b79
Antonin <antonin.cavanne@outlook.fr>
2023-08-04 11:33:33 +0200

    supppression du maker bundle en mode dev

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,18 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    //on utilise makerbundle uniquement en dev
+    public function registerBundles(): iterable
+    {
+        $bundles = [
+            //... liste de vos autres bundles
+        ];
+
+        if ($this->getEnvironment() === 'dev') {
+            $bundles[] = new MakerBundle();
+        }
+
+        return $bundles;
+    }
 }
