eb6acbceaa705bf4dc3ad2b802488983461ad122
Antonin <antonin.cavanne@outlook.fr>
2023-08-29 10:30:48 +0200

    responsive espace pro et mise en place du isgranted sur espace-professionnel

--- src/Controller/EspaceProController.php
+++ src/Controller/EspaceProController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EspaceProController extends AbstractController
 {
     #[Route('/espace-professionnel', name: 'espcepro')]
+    #[IsGranted('ROLE_USER')]
     public function index(): Response
     {
         return $this->render('espace_pro/espacepro.html.twig', []);
