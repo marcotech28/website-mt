cb8e0f2df7fb0984cd3441cdf25e46c3e1863e3a
marcotech28 <benjamin@marconnet-technologies.com>
2023-09-08 15:47:25 +0200

    marque url

--- src/Form/MarqueType.php
+++ src/Form/MarqueType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class MarqueType extends AbstractType
 {
