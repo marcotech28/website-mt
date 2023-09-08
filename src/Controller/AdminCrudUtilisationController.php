095118db90c3ce3e0b4dcde0d69985d7e6f40ef3
marcotech28 <benjamin@marconnet-technologies.com>
2023-09-08 15:19:23 +0200

    utilisation url

--- src/Controller/AdminCrudUtilisationController.php
+++ src/Controller/AdminCrudUtilisationController.php
@@ -57,9 +57,7 @@ class AdminCrudUtilisationController extends AbstractController
     #[Route('/{id}/edit', name: 'app_admin_crud_utilisation_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Utilisation $utilisation, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(UtilisationType::class, $utilisation, [
-            'image_filename' => $utilisation->getImage() // ou la méthode que vous utilisez pour obtenir le nom du fichier
-        ]);
+        $form = $this->createForm(UtilisationType::class, $utilisation);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
