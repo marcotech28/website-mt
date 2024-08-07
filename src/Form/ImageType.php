b41751446951ee9ee325d7468eedbdce9db2027a
37anton <antonin.cavanne@outlook.fr>
2024-08-07 23:59:19 +0200

    feat: creation formulaire image

---
+++ src/Form/ImageType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class ImageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('url', TextType::class, [
+                'label' => 'URL de l\'image',
+                'attr' => [
+                    'placeholder' => 'Entrez l\'URL de l\'image'
+                ]
+            ])
+            ->add('alt', TextType::class, [
+                'label' => 'Texte alt',
+                'attr' => [
+                    'placeholder' => 'Entrez le texte alt pour l\'image'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
