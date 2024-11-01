1fc1f282af48ff35fefa95b5ebfc95d15b095908
37anton <antonin.cavanne@outlook.fr>
2024-11-01 10:02:32 +0100

    formulaires email

--- src/Controller/DemonstrationController.php
+++ src/Controller/DemonstrationController.php
@@ -51,8 +51,8 @@ class DemonstrationController extends AbstractController
             $message = $data['message'];
 
             $monemail = (new Email())
-                ->from('communication@marconnet-technologies.com')
-                ->to('communication@marconnet-technologies.com')
+                ->from('info@marconnet-robotique.com')
+                ->to('info@marconnet-robotique.com')
                 ->subject('Demande de démonstration')
                 ->html("
                     <p><strong>Type d'utilisateur:</strong> {$typeUser}</p>
