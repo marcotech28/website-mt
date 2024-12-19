f7f10529d2b00b551136b0185a302a8f06365288
37anton <antonin.cavanne@outlook.fr>
2024-12-20 00:45:42 +0100

    Feat: arret envoi emails en environnement de dev

--- src/Service/EmailService.php
+++ src/Service/EmailService.php
@@ -4,14 +4,17 @@ namespace App\Service;
 
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class EmailService
 {
     private $mailer;
+    private $environment;
 
-    public function __construct(MailerInterface $mailer)
+    public function __construct(MailerInterface $mailer, KernelInterface $kernel)
     {
         $this->mailer = $mailer;
+        $this->environment = $kernel->getEnvironment();
     }
 
     /**
@@ -25,6 +28,13 @@ class EmailService
      */
     public function sendEmail(string $to, string $subject, string $htmlContent, ?string $from = null): void
     {
+
+        if ($this->environment !== 'prod') {
+            // Logique pour les environnements autres que prod (par exemple, logs ou retour)
+            error_log("Envoi d'email simulé (non envoyé en {$this->environment}). Sujet : $subject");
+            return; // Ne pas envoyer l'email
+        }
+
         $email = (new Email())
             ->from($from ?? 'info@marconnet-robotique.com') // Utiliser l'expéditeur par défaut si non fourni
             ->to($to)
